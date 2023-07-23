363a557cadd6f9d492fa973464cb445567946543
Mrick343 <mrinmoy8429de@gmail.com>
2023-07-23 20:51:34 +0530

    Add Evolution Trees Script

--- evo_trees.sh
+++ evo_trees.sh
@@ -5,7 +5,7 @@ clear
 echo "=================================="
 echo "|                                |"
 echo "|  OnePlus 9RT 5G Setup Script   |"
-echo "|         Branch: blaze-13.1     |"
+echo "|         Branch: evo-13.1       |"
 echo "|  Maintained By: Mrick343       |"
 echo "|                                |"
 echo "=================================="
@@ -14,10 +14,10 @@ echo "=================================="
 banner 2>1
 
 # Clone Device Tree
-git clone -b blaze-13.1 https://github.com/Mrick343/android_device_oneplus_martini.git device/oneplus/martini
+git clone -b evo-13.1 https://github.com/Mrick343/android_device_oneplus_martini.git device/oneplus/martini
 
 # Clone Common Tree
-git clone -b blaze-13.1 https://github.com/Mrick343/android_device_oneplus_sm8350-common.git device/oneplus/sm8350-common
+git clone -b evo-13.1 https://github.com/Mrick343/android_device_oneplus_sm8350-common.git device/oneplus/sm8350-common
 
 # Clone Device Vendor
 git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplus_martini.git vendor/oneplus/martini
@@ -26,11 +26,14 @@ git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplu
 git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplus_sm8350-common.git vendor/oneplus/sm8350-common
 
 # Cloning kernel tree
-git clone -b droidx-13.1 https://github.com/Mrick343/android_kernel_oneplus_sm8350.git kernel/oneplus/sm8350
+git clone -b thirteen-pelt https://github.com/Mrick343/android_kernel_oneplus_sm8350.git kernel/oneplus/sm8350
 
 # Clone hardware
-git clone -b droidx-13.1 https://github.com/Mrick343/hardware_oneplus.git hardware/oplus
+git clone -b evo-13.1 https://github.com/Mrick343/hardware_oneplus.git hardware/oplus
 
 # Clone Camera
 git clone -b 13.0 https://gitlab.com/PixelOS-Devices/proprietary_vendor_oplus_camera vendor/oplus/camera
+
+#Clone OPlusExtras
+git clone https://github.com/Evolution-X-Devices/packages_apps_OPlusExtras.git packages/apps/OPlusExtras/
 
