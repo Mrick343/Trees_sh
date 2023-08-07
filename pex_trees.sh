263b8001d5c51c6ac2e0f1991782953ebd540db0
Mrick343 <mrinmoy8429de@gmail.com>
2023-08-07 18:32:43 +0530

    Update branches

--- pex_trees.sh
+++ pex_trees.sh
@@ -14,10 +14,10 @@ echo "=================================="
 banner 2>1
 
 # Clone Device Tree
-git clone -b pex-13.1 https://github.com/Mrick343/android_device_oneplus_martini.git device/oneplus/martini
+git clone -b thunder https://github.com/Mrick343/android_device_oneplus_martini.git device/oneplus/martini
 
 # Clone Common Tree
-git clone -b pex-13.1 https://github.com/Mrick343/android_device_oneplus_sm8350-common.git device/oneplus/sm8350-common
+git clone -b thunder https://github.com/Mrick343/android_device_oneplus_sm8350-common.git device/oneplus/sm8350-common
 
 # Clone Device Vendor
 git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplus_martini.git vendor/oneplus/martini
@@ -26,11 +26,11 @@ git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplu
 git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplus_sm8350-common.git vendor/oneplus/sm8350-common
 
 # Cloning kernel tree
-git clone -b thirteen-pelt https://github.com/Mrick343/android_kernel_oneplus_sm8350.git kernel/oneplus/sm8350
+git clone -b thirteen-fine https://github.com/Mrick343/android_kernel_oneplus_sm8350.git kernel/oneplus/sm8350
 
 # Clone hardware
 git clone -b pex-13.1 https://github.com/Mrick343/hardware_oneplus.git hardware/oplus
 
 # Clone Camera
-git clone -b 13.0 https://gitlab.com/PixelOS-Devices/proprietary_vendor_oplus_camera vendor/oplus/camera
+git clone -b 13.0 https://gitlab.com/Mrick343/proprietary_vendor_oplus_camera.git vendor/oplus/camera
 
