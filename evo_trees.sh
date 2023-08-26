868f9543c4db13529d2634a418e06721cbc3c171
Mrick343 <mrinmoy8429de@gmail.com>
2023-08-26 22:07:03 +0530

    Update script

--- evo_trees.sh
+++ evo_trees.sh
@@ -26,13 +26,16 @@ git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplu
 git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplus_sm8350-common.git vendor/oneplus/sm8350-common
 
 # Cloning kernel tree
-git clone -b thirteen-pelt https://github.com/Mrick343/android_kernel_oneplus_sm8350.git kernel/oneplus/sm8350
+git clone -b thirteen-fine https://github.com/Mrick343/android_kernel_oneplus_sm8350.git kernel/oneplus/sm8350
 
 # Clone hardware
 git clone -b evo-13.1 https://github.com/Mrick343/hardware_oneplus.git hardware/oplus
 
 # Clone Camera
 git clone -b 13.0 https://gitlab.com/PixelOS-Devices/proprietary_vendor_oplus_camera vendor/oplus/camera
+
+#Clone Firmware
+git clone -b 13.1 https://gitlab.com/Mrick343/vendor_firmware_martini.git vendor/firmware
 
 #Clone OPlusExtras
 git clone https://github.com/Evolution-X-Devices/packages_apps_OPlusExtras.git packages/apps/OPlusExtras/
