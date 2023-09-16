fcf56cab9da6f50e7736f454f439361fcba2e680
Mrick343 <mrinmoy8429de@gmail.com>
2023-09-16 21:47:17 +0530

    Update Branches & repos

--- droidx_trees.sh
+++ droidx_trees.sh
@@ -14,19 +14,19 @@ echo "=================================="
 banner 2>1
 
 # Clone Device Tree
-git clone -b limetree git@github.com:Mrick343/device_oneplus_martini-pr.git device/oneplus/martini
+git clone -b limedust https://github.com/Mrick343/android_device_oneplus_martini.git device/oneplus/martini
 
 # Clone Common Tree
-git clone -b limetree git@github.com:Mrick343/android_device_oneplus_sm8350-common-pr.git device/oneplus/sm8350-common
+git clone -b limedust https://github.com/Mrick343/android_device_oneplus_sm8350-common.git device/oneplus/sm8350-common
 
 # Clone Device Vendor
-git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplus_martini.git vendor/oneplus/martini
+git clone -b thirteen https://github.com/Mrick343/proprietary_vendor_oneplus_martini.git vendor/oneplus/martini
 
 # Clone Common Vendor
-git clone -b thirteen-june https://github.com/Mrick343/proprietary_vendor_oneplus_sm8350-common.git vendor/oneplus/sm8350-common
+git clone -b thirteen https://github.com/Mrick343/proprietary_vendor_oneplus_sm8350-common.git vendor/oneplus/sm8350-common
 
 # Cloning kernel tree
-git clone -b thirteen-fine git@github.com:Mrick343/android_kernel_oneplus_sm8350.git kernel/oneplus/sm8350
+git clone -b rebase-3 git@github.com:Mrick343/android_kernel_oneplus_sm8350.git kernel/oneplus/sm8350
 
 # Clone hardware
 git clone -b droidx-13.1 https://github.com/Mrick343/hardware_oneplus.git hardware/oplus
