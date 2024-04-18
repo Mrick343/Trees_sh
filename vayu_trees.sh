835abb265605fbedfe596aa08e4fdeeec8038cc4
Mrinmoy Dey <mrinmoy8429de@gmail.com>
2024-04-18 21:38:09 +0530

    Update vayu_trees.sh

--- vayu_trees.sh
+++ vayu_trees.sh
@@ -14,19 +14,19 @@ echo "=================================="
 banner 2>1
 
 echo Cloning Device Tree
-git clone -b 13.0 https://github.com/Mrick-stuffs/android_device_xiaomi_vayu.git device/xiaomi/vayu
+git clone -b 14.0 https://github.com/Mrick-stuffs/android_device_xiaomi_vayu.git device/xiaomi/vayu
 echo
 echo Cloning Common Tree
-git clone -b 13.0 https://github.com/Mrick-stuffs/android_device_xiaomi_sm8150-common.git device/xiaomi/sm8150-common
+git clone -b 14.0 https://github.com/Mrick-stuffs/android_device_xiaomi_sm8150-common.git device/xiaomi/sm8150-common
 echo
 echo Cloning Vendor
-git clone -b 13.0 https://gitlab.com/Mrick343/proprietary_vendor_xiaomi.git vendor/xiaomi
+git clone -b 14.0 https://gitlab.com/Mrick343/proprietary_vendor_xiaomi.git vendor/xiaomi
 echo
 echo Cloning kernel tree
-git clone -b 13.0 https://github.com/Mrick-stuffs/android_kernel_xiaomi_sm8150.git kernel/xiaomi/sm8150
+git clone -b main https://github.com/GXC2356/platform_kernel_xiaomi_vayu.git kernel/xiaomi/sm8150
 echo
 echo Cloning hardware_xiaomi
-git clone -b lineage-20 https://github.com/Mrick-stuffs/android_hardware_xiaomi.git hardware/xiaomi
+git clone -b lineage-21 https://github.com/Mrick-stuffs/android_hardware_xiaomi.git hardware/xiaomi
 echo
-echo Cloning Clang-17
-git clone -b 13.0 https://gitlab.com/Mrick343/android_prebuilts_clang_host_linux-x86_clang-r498229b.git prebuilts/clang/host/linux-x86/clang-r498229b
+#echo Cloning Clang-17
+#git clone -b 13.0 https://gitlab.com/Mrick343/android_prebuilts_clang_host_linux-x86_clang-r498229b.git prebuilts/clang/host/linux-x86/clang-r498229b
